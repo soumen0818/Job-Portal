bc5ef9a9e9721a5f8909dc0e197d69ae9ae8ce25
Soumen Das <sdas721444@gmail.com>
2025-07-30 09:43:07 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Button>Login</Button>
+    </div>
+  )
+}
+
+export default App
