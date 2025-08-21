30c32f64fc04289acb4f495bd21add2c6a757fa4
Soumen Das <sdas721444@gmail.com>
2025-08-21 22:08:06 +0530

    Refactor App.jsx to implement routing and enhance application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Button } from "./components/ui/button";
 import React from 'react'
 import './App.css'
+import { ThemeProvider } from "./components/theme-provider";
+import AppLayout from "./layout/app-layout";
+import Landing from "./pages/Landing";
+import Onboarding from "./pages/onboarding";
+import JobListing from "./pages/job-listing";
+import JobApplicationPage from "./pages/job-application";
+import JobPost from "./pages/job-post";
+import SavedJob from "./pages/all-job";
+import MyJob from "./pages/my-job";
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />
+      },
+      {
+        path: '/onboarding',
+        element: <Onboarding />
+      },
+      {
+        path: '/jobs',
+        element: <JobListing />
+      },
+      {
+        path: '/job/:id',
+        element: <JobApplicationPage />
+      },
+      {
+        path: '/post-job',
+        element: <JobPost />
+      },
+      {
+        path: '/saved-job',
+        element: <SavedJob />
+      },
+      {
+        path: '/my-job',
+        element: <MyJob />
+      },
+    ]
+  }
+])
 
 function App() {
-  
 
-  return (
-    <div>
-      <Button>Login</Button>
-    </div>
-  )
+  return(
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  ); 
 }
 
 export default App
