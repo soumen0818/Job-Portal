040324a7cc75f1808bd75771a73533eba9f7a18f
Soumen Das <sdas721444@gmail.com>
2025-08-23 23:08:41 +0530

    Refactor Header component layout and update image size

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 function Header() {
   return (
     <>
-        <nav className='py-4 flex justify-between items-center'>
-          <Link>
-            <img src="/Hireura.png" alt="logo" className='h-20 ' />
-          </Link>
+      <nav className='py-4 flex justify-between items-center'>
+        <Link className="pl-6">
+          <img src="/Hireura.png" alt="logo" className='h-15 ' />
+        </Link>
 
-          <Button variant = "outline">Login</Button>
-        </nav>
+        <Button variant="outline">Login</Button>
+        {/* <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn> */}
+      </nav>
     </>
   );
-    
-  
+
+
 };
 
 export default Header
