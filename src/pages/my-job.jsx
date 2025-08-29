f2ffe79f4a24b39e2125da6c568f06c68de4d27c
Soumen Das <sdas721444@gmail.com>
2025-08-29 22:35:48 +0530

    refactor MyJob component to utilize user role for dynamic content rendering

--- src/pages/my-job.jsx
+++ src/pages/my-job.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import CreatedApplications from "@/components/created-application";
+import CreatedJobs from "@/components/created-jobs";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
 
-function MyJob() {
+const MyJob = () => {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
   return (
-    <div>MyJob</div>
-  )
-}
+    <div>
+      <h1 className="gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8">
+        {user?.unsafeMetadata?.role === "candidate"
+          ? "My Applications"
+          : "My Jobs"}
+      </h1>
+      {user?.unsafeMetadata?.role === "candidate" ? (
+        <CreatedApplications />
+      ) : (
+        <CreatedJobs />
+      )}
+    </div>
+  );
+};
 
-export default MyJob
+export default MyJob;
